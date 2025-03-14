c566d6cdecbf20db08cf7866f21cfcf0e1b144db
MrTorabekov <diyorbektorabekov08@gmail.com>
2025-03-14 11:53:01 +0500

    commit

--- database.py
+++ database.py
@@ -2,16 +2,16 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
+load_dotenv('.env')
 
 conn = psycopg2.connect(
-    dbname="fastapi",
-    user="postgres",
-    password="2008",
-    host="localhost",
-    port="5432"
+    database=os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT')
 )
+
 cursor = conn.cursor()
 
 cursor.execute("""
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from models import Users
 from database import Database
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('.env')
 
 db = Database(
-    db_file='fastapi',
-    user='postgres',
-    password='2008',
-    host='localhost',
-    port='5432'
+    db_file=os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    host=os.getenv('DB_HOST'),
+    port=os.getenv('DB_PORT')
 )
 
 app = FastAPI()
